377553db1d927d1aabd74145060951e4038b25b8
Marcin Bojara <marcin_5@mail.ru>
2023-12-26 17:04:42 +0100

    add styling to show snippet page

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -16,5 +16,18 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
 
   if (!snippet) return notFound();
 
-  return <div>{snippet.title}</div>;
+  return (
+    <div>
+      <div className="flex m-4 justify-between items-center">
+        <h1 className="text-xl text-bold">{snippet.title}</h1>
+        <div className="flex gap-4">
+          <button className="p-2 border rounded">Edit</button>
+          <button className="p-2 border rounded">Delete</button>
+        </div>
+      </div>
+      <pre className="p-3 border rounded bg-gray-200 border-neutral-200">
+        <code>{snippet.code}</code>
+      </pre>
+    </div>
+  );
 }

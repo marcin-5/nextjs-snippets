8c1febb09b8d1deec72f1945e53ec407d4187095
Marcin Bojara <marcin_5@mail.ru>
2023-12-28 13:52:23 +0100

    update snippet

--- src/components/snippet-edit-form.tsx
+++ src/components/snippet-edit-form.tsx
@@ -3,6 +3,7 @@
 import { Editor } from '@monaco-editor/react';
 import type { Snippet } from '@prisma/client';
 import { useState } from 'react';
+import * as actions from '@/actions';
 
 interface SnippetEditFormProps {
   snippet: Snippet;
@@ -10,9 +11,12 @@ interface SnippetEditFormProps {
 
 export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
   const [code, setCode] = useState(snippet.code);
+
   const handleEditorChange = (value: string = '') => {
     setCode(value);
   };
+
+  const editSnippetAction = actions.editSnippet.bind(null, snippet.id, code);
 
   return (
     <div>
@@ -24,6 +28,11 @@ export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
         options={{ minimap: { enabled: false } }}
         onChange={handleEditorChange}
       />
+      <form action={editSnippetAction}>
+        <button type="submit" className="p-2 border rounded">
+          Save
+        </button>
+      </form>
     </div>
   );
 }
